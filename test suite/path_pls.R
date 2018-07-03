b0898fbbc628a751449d9226bedc55b3b353ce46
Roel Bouman <roelbouman@gmail.com>
2018-07-03 10:14:39 +0200

    Added initial skeleton and basic requirements for Path PLS

---
+++ test suite/path_pls.R
@@ -0,0 +1,10 @@
+path_pls <- function(data, connection_matrix, vars_in_block
+                     #component_selection="auto", n_comps=NULL,
+                     #sub_blocks=FALSE, sub_block_assignment=NULL,
+                     #scaling="standardize",
+                     #bootstrap="FALSE", bootstrap_iter=NULL
+                     ){
+
+
+
+}
