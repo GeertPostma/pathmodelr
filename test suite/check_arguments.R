e6d02996fb281428eeef302a008c89ee232e4285
RoelBouman <roelbouman@gmail.com>
2018-07-06 14:33:48 +0200

    Added skeleton for check_arguments function.

---
+++ test suite/check_arguments.R
@@ -0,0 +1,24 @@
+check_arguments <- function(data, connection_matrix, vars_in_block
+                            #component_selection="auto", n_comps=NULL,
+                            #sub_blocks=FALSE, sub_block_assignment=NULL, sub_block_scaling_method=NULL
+                            #preprocessing settings: standardizing, mean-centering, {Assign per block, include scaling for categorical variables and spectra}
+                            #input_variable_type: assigns what type each different variable has
+                            #bootstrap="FALSE", bootstrap_iter=NULL
+){
+  
+  
+  #Is the graph a DAG?
+  
+  
+  #Is data numeric?
+  
+  
+  #Are all variables assigned? Are all named variables in the data?
+  
+  
+  #Are all paired optional parameters set if one is set?
+  
+  
+  are_valid = TRUE
+}
+  
--- test suite/path_pls.R
+++ test suite/path_pls.R
@@ -7,10 +7,13 @@ path_pls <- function(data, connection_matrix, vars_in_block
                      ){
 
   ##CHECK INPUT
-  #Is the graph a DAG?
-  #Is data numeric?
-  #Are all variables assigned? Are all named variables in the data?
-  #Are all paired optional parameters set if one is set?
+  check_arguments(data, connection_matrix, vars_in_block
+                  #component_selection="auto", n_comps=NULL,
+                  #sub_blocks=FALSE, sub_block_assignment=NULL, sub_block_scaling_method=NULL
+                  #preprocessing settings: standardizing, mean-centering, {Assign per block, include scaling for categorical variables and spectra}
+                  #input_variable_type: assigns what type each different variable has
+                  #bootstrap="FALSE", bootstrap_iter=NULL
+  )
 
   ##Pre-Process
 
