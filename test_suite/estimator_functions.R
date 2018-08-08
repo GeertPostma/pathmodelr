abfc15d462de03b0a11c2e4a6c7f3169d81e3eb4
Roel Bouman <roelbouman@gmail.com>
2018-08-08 14:43:42 +0200

    Added basic functionality for PLS estimator, n_LVs selection still misses.

--- test_suite/estimator_functions.R
+++ test_suite/estimator_functions.R
@@ -1,6 +1,31 @@
 #All estimators must call add_estimate!
 
+#Also updates same level and next level nodes
 PLS_estimator <- function(node){ 
+  #TODO: Add cross-validation n_LVs selection
+  
+  n_LVs <- 2
+  
+  #final result step:
+  combined_and_masked <- combine_and_mask(node)
+  
+  X <- combined_and_masked$X
+  Y <- combined_and_masked$Y
+  covariance_mask <- combined_and_masked$covariance_mask
+  cols_per_X_node <- combined_and_masked$cols_per_X_node
+  same_level_nodes <- combined_and_masked$same_level_nodes
+  
+  X_loadings <- SIMPLS(X,Y, max_n_comp=n_LVs, minimal=TRUE, covariance_mask=covariance_mask)$X_loadings
+
+  for(i in seq_along(same_level_nodes)){
+    update_node <- same_level_nodes[[i]]
+    node_cols <- cols_per_X_node[[i]]
+    
+    node_loadings <- X_loadings[node_cols,]
+    node_LVs <- update_node$preprocessed_X %*% node_loadings
+    
+    update_node$add_estimate(n_LVs, node_LVs, node_loadings)
+  }
   
 }
 
