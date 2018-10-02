1dea09a35292fb3b139b07e5628929c03e0540dc
Roel Bouman <roelbouman@gmail.com>
2018-10-02 15:16:36 +0200

    Fixed bug when post_processing nodes with a single LV.

--- R/post_processing_functions.R
+++ R/post_processing_functions.R
@@ -1,7 +1,12 @@
 # Post processor function for standardizing LVs
 standardize_LVs <- function(node){
 
-  scaling_settings <- apply(node$LVs, 2, sd)
 
+  if(dim(node$LVs)[2] > 1){
+    scaling_settings <- apply(node$LVs, 2, sd)
+  }
+  else{
+    scaling_settings <- sd(node$LVs)
+  }
   return(scaling_settings)
 }
