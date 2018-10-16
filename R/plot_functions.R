36254b03193506bc94f9aec4660270f206ab68ea
Roel Bouman <roelbouman@gmail.com>
2018-10-16 10:39:33 +0200

    Added plot_inner_model function and adjusted arrow placement values.

--- R/plot_functions.R
+++ R/plot_functions.R
@@ -16,7 +16,27 @@ plot_variances <- function(model,
          label = TRUE,
          edge.label = "weights",
          arrow.size = 10,
-         arrow.gap = 0.04,
-         size = 20,
-         mode="circle")
+         arrow.gap = 0.06,
+         size = 30,
+         mode=mode)
+}
+
+#' @import ggplot2
+#' @import GGally
+#' @import sna
+#' @importFrom network as.network
+#' @export
+plot_inner_model <- function(model,
+                           mode="circle"){
+
+  net <- as.network(as.data.frame(model$connection_matrix),
+                    directed = TRUE,
+                    matrix_type = "incidence")
+
+  ggnet2(net,
+         label = TRUE,
+         arrow.size = 10,
+         arrow.gap = 0.06,
+         size = 30,
+         mode=mode)
 }
