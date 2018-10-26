95afae44c731a4a158eb9c969ffe311a530ccfe4
Roel Bouman <roelbouman@gmail.com>
2018-10-26 12:55:30 +0200

    Created first working version of plot_variable_effects which plots the effects of variables on one or more blocks.

--- R/plot_functions.R
+++ R/plot_functions.R
@@ -43,27 +43,52 @@ plot_inner_model <- function(model,
          mode=mode)
 }
 
-plot_variable_effects <- function(model){
+#Default: show all end nodes, give name
+#if input is list of names: plot all
+#' @import ggplot2
+#' @importFrom dplyr bind_rows
+#' @importFrom dplyr inner_join
+#' @export
+plot_variable_effects <- function(model, what_node=NULL){
+
+  #what_node="Product"
+
+  if(is.null(what_node)){
+    what_node <- unlist(lapply(model$nodes, function(node) if(node$node_type=="End") node$node_name))
+  }
+
+  plots <- list()
+  for(study_node in what_node){
+    effects <- model$variable_effects$effects[[study_node]]
+
+    var_names <- names(effects)
+
+    plot_df <- data.frame(name=var_names, effects=effects, row.names=NULL, stringsAsFactors=FALSE)
+
+    #get all vars in blocks:
+    vars_in_block <- bind_rows(lapply(model$nodes, function(node) data.frame(rep(node$node_name, times=length(colnames(node$X_data))), colnames(node$X_data), stringsAsFactors=FALSE)))
+
+    colnames(vars_in_block) <- c("block", "name")
+
+    plot_df <- inner_join(plot_df, vars_in_block, by = "name")
+
+    plots[[study_node]] <- ggplot(data = plot_df,
+
+      aes(x = reorder(name, 1:length(name)), y = abs(effects), fill = block)) +
+
+      geom_bar(stat = 'identity', position = 'dodge') +
+
+      theme(axis.text.x = element_text(angle = 270)) +
+
+      ylab("Absolute Effect") +
+
+      xlab("Variable name") +
+
+      ggtitle(study_node)
 
 
+  }
+  return(plots)
 
-
-
-
-  ggplot(data = model,
-
-              aes(x = name, y = abs(weight), fill = block)) +
-
-    geom_bar(stat = 'identity', position = 'dodge') +
-
-    # add title
-
-    # ggtitle(paste("Barchart of", attr(model, "method"), attr(model, "what"), attr(model, "data"))) +
-
-    # rotate x-axis names
-
-    theme(axis.text.x = element_text(angle = 90)) +
-
-    ylab("Absolute Effect")
 }
 
