8f44acf86a72d4fc4a4c36af2670982685590c86
Roel Bouman <roelbouman@gmail.com>
2018-11-08 10:24:32 +0100

    Fixed incorrect ordering bug in get_path_ci.

--- R/get_ci.R
+++ R/get_ci.R
@@ -1,6 +1,6 @@
 #help function for getting the ci
-get_ci <- function(result_matrix, bootstrap_iter, bootstrap_ci){
-  ordered_matrix <- apply(result_matrix, c(1,2), order)
+get_path_ci <- function(result_matrix, bootstrap_iter, bootstrap_ci){
+  ordered_matrix <- aperm (apply (result_matrix, 1:2, sort), c(2, 3, 1))
   lower_index <- ceiling((bootstrap_iter * (1-bootstrap_ci))/2)
   upper_index <- bootstrap_iter-lower_index
 
