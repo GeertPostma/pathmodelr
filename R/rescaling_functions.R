510bc4a7a76fca568d32ffd649539650be1980e0
Roel Bouman <roelbouman@gmail.com>
2018-11-26 16:43:16 +0100

    Added scaling functions for rescaling of LVs and weights for block and/or variance scaling.

---
+++ R/rescaling_functions.R
@@ -0,0 +1,24 @@
+
+get_scale_vec <- function(LVs, variance_explained, block_scale=FALSE, variance_scale=FALSE){
+
+  scale_vec <- rep(1, length(variance_explained))
+
+  if(variance_scale){
+    scale_vec <- scale_vec * sqrt(scale_vec * variance_explained)
+  }
+
+  if(block_scale){
+    SS <- sum((LVs %*% scale_vec)^2)
+    scale_vec <- scale_vec / sqrt(SS)
+  }
+
+  return(scale_vec)
+}
+
+rescale_X_weights <- function(X_weights, scale_vec){
+  return(X_weights %*% diag(scale_vec))
+}
+
+rescale_LVs <- function(LVs, scale_vec){
+  return(LVs %*% diag(scale_vec))
+}
