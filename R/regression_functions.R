2d9716d13bd34e228843f996256725637fdac663
Roel Bouman <roelbouman@gmail.com>
2018-11-26 16:45:49 +0100

    Updated SIMPLS to be faster and return variance explained.

--- R/regression_functions.R
+++ R/regression_functions.R
@@ -3,9 +3,7 @@
 #follows original algorithm described by De Jong (uses same nomenclature) (T and t are replaced by TT and tt to avoid overloading of TRUE and transpose aliases in R)
 #assumes all relevant preprocessing is already done (including mean-centering!)
 #Includes minimal version for when only predictions and loadings are needed
-SIMPLS <- function(X,Y, max_n_comp=ncol(X), minimal=FALSE, covariance_mask=NULL){
-  X <- as.matrix(X)
-  Y <- as.matrix(Y)
+SIMPLS <- function(X,Y, max_n_comp=ncol(X), minimal=FALSE){
 
   V <- R <- matrix(0, nrow=ncol(X), ncol=max_n_comp)
   B <- array(0 , dim=c(ncol(X), ncol(Y), max_n_comp))
@@ -17,10 +15,6 @@ SIMPLS <- function(X,Y, max_n_comp=ncol(X), minimal=FALSE, covariance_mask=NULL)
   }
 
   S <- crossprod(X,Y)     #Cross product
-
-  if(!is.null(covariance_mask)){
-    S <- S * covariance_mask
-  }
 
   for(a in 1:max_n_comp){
     q <- svd(S)$v[,1]
@@ -61,11 +55,13 @@ SIMPLS <- function(X,Y, max_n_comp=ncol(X), minimal=FALSE, covariance_mask=NULL)
 
       P[, a] <- p
       U[, a] <- u
-
     }
 
   }
-
+  if(!minimal){
+    X_variance_explained <- diag(crossprod(P, P)) / (sum(X^2))
+    Y_variance_explained <- diag(crossprod(Q, Q)) / (sum(Y^2))
+  }
   if(minimal){
     return(list("X_loadings"=V,
                 "Y_loadings"=Q,
@@ -79,6 +75,8 @@ SIMPLS <- function(X,Y, max_n_comp=ncol(X), minimal=FALSE, covariance_mask=NULL)
                 "X_scores"=TT,
                 "Y_scores"=U,
                 "X_loadings_unorthogonalized"=P,
-                "X_weights"=R))
+                "X_weights"=R,
+                "X_variance_explained"=X_variance_explained,
+                "Y_variance_explained"=Y_variance_explained))
   }
 }
