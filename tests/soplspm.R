77c978156cf35c74af4971622bfafb37c6f1706a
Unknown <roelbouman@gmail.com>
2019-08-22 12:47:13 +0200

    Added SOPLSPM test with manual setting of n_LVs

--- tests/soplspm.R
+++ tests/soplspm.R
@@ -18,3 +18,13 @@ colnames(inner_model_spec) <- rownames(inner_model_spec)
 outer_model_spec <- list(1:5, 6:8, 9:18, 19:27, 28)
 
 soplsm_model <- soplspm(wine_data, inner_model_spec, outer_model_spec)
+
+#make second model with single LV per block:
+n_LVs <- t(matrix(c(0,0,0,0,0,
+                    1,0,0,0,0,
+                    1,1,0,0,0,
+                    1,1,1,0,0,
+                    1,1,1,1,0),
+                  nrow=5, ncol=5))
+
+soplspm_model <- soplspm(wine_data, inner_model_spec, outer_model_spec, n_LVs=n_LVs)
