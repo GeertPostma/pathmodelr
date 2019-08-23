60cd0115fbc59c3a109e63a857901a936378c558
Unknown <roelbouman@gmail.com>
2019-08-23 10:24:29 +0200

    Fixed capital letter spelling mistake in SOPLS orthogonalize function

--- R/SOPLS_orthogonalize.R
+++ R/SOPLS_orthogonalize.R
@@ -2,5 +2,5 @@ SOPLS_orthogonalize <- function(X_2, T_1){
 
   X_2_orth <- (diag(dim(T_1)[[1]]) - tcrossprod(T_1 %*% solve(crossprod(T_1, T_1)), T_1)) %*% X_2
 
-  return(x_2_orth)
+  return(X_2_orth)
 }
